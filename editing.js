948cb3a6d5a1afc7bfdbec4ad9961e75572f0c8f
SHASWATA  DAS <96969805+rikuzavi@users.noreply.github.com>
2026-01-01 13:29:41 +0530

    Update editing.js

--- editing.js
+++ editing.js
@@ -893,7 +893,7 @@ document.getElementById("downloadimg").addEventListener("click", async () => {
   </body>
   </html>`;
 
-  const res = await fetch("https://phomerang-backend-m75dw8ame-shaswata-das-projects-54d16cf6.vercel.app/api/render", {
+  const res = await fetch("https://phomerang-backend.vercel.app/api/render", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ html })
@@ -910,4 +910,5 @@ document.getElementById("downloadimg").addEventListener("click", async () => {
   document.getElementById('downloadimg').classList.replace(document.getElementById('downloadimg').classList[2],"fa-download")
   document.getElementById('downloadimg').classList.replace(document.getElementById('downloadimg').classList[3],"none")
 });
+
 
